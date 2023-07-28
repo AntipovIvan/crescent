5ee3675a79002d9ba1dbcb980a5f24f253998fe7
AntipovIvan <85480386+AntipovIvan@users.noreply.github.com>
2023-07-28 15:36:31 +0900

    test

--- crescent/crescentSite/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class YourModel(models.Model):
-    field1 = models.CharField(max_length=100)
-    field2 = models.IntegerField()
-    # Add new fields below
-    additional_field1 = models.BooleanField(default=False)
-    additional_field2 = models.DateField()
