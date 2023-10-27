d41d30ac158ad74f88d4f6a411bd70d385460114
AntipovIvan <85480386+AntipovIvan@users.noreply.github.com>
2023-10-27 11:41:24 +0900

    local dev

--- front/src/pages/Home.svelte
+++ front/src/pages/Home.svelte
@@ -19,7 +19,8 @@
 
 	onMount(async () => {
 		try {
-			const response = await fetch('http://52.69.50.8:7000/api/newsmodels');
+			// const response = await fetch('http://52.69.50.8:7000/api/newsmodels');
+			const response = await fetch('http://localhost:7000/api/newsmodels');
 			if (!response.ok) {
 				throw new Error('Network response was not ok');
 			}
