c0c65d5394aa89e2a2dad5ab0218ee6dd2ed8835
AntipovIvan <85480386+AntipovIvan@users.noreply.github.com>
2023-11-07 17:20:28 +0900

    nginx check

--- back/crescent/settings.py
+++ back/crescent/settings.py
@@ -153,13 +153,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-# STATIC_ROOT = Path(BASE_DIR, "static")
+STATIC_ROOT = Path(BASE_DIR, "static")
 
 MEDIA_URL = "/media/"
-# MEDIA_ROOT = Path(BASE_DIR, "media")
-
-STATIC_ROOT = "/var/www/static"
-MEDIA_ROOT = "/var/www/media"
+MEDIA_ROOT = Path(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
