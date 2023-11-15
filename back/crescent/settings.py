631d9959b4f92bbc4cef3bebe8124cfb47fad72d
AntipovIvan <85480386+AntipovIvan@users.noreply.github.com>
2023-11-15 18:48:08 +0900

    debug false

--- back/crescent/settings.py
+++ back/crescent/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "42le!p&pkezf(n#ymp6j7s&&t!@uv@r2yd!_vvfu62v+dg6fb)"
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
